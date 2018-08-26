46db6e066fe7805c509894400fb788d06dd34913
sjanisz <szymon.janisz.et.di@gmail.com>
2018-08-26 11:44:25 +0200

    Bin output changed
    Changed web api build directory (debug and release) to bin\ due to webapi2 build restrictions (e.g. https://stackoverflow.com/questions/29852630/change-build-output-directory-of-webapi-application; https://stackoverflow.com/questions/41139748/how-to-correctly-do-debug-release-builds-in-asp-net-web-api-2-app;).

--- server/src/ShopManager/Controllers/UserController.cs
+++ server/src/ShopManager/Controllers/UserController.cs
@@ -11,7 +11,12 @@ namespace ShopManager.Controllers
     {
         public string GetUsers()
         {
-            return "users...";
+            return "users...dd";
+        }
+
+        public string Get(int a)
+        {
+            return a.ToString();
         }
     }
 }
